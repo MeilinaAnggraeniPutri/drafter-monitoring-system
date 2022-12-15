efe54ff7961ccdf4b6a7936239cb2c61a53bd19b
b0rnt0ber00t <lanabackend@gmail.com>
2022-12-15 07:17:04 +0000

    update topbar

--- modules/Dashboard/resources/views/components/topbar.blade.php
+++ modules/Dashboard/resources/views/components/topbar.blade.php
@@ -245,7 +245,7 @@
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="{{ route('user.profile.index') }}">
                         <i class="bx bx-user font-size-16 align-middle me-1"></i>
                         <span key="t-profile">Profile</span>
                     </a>
