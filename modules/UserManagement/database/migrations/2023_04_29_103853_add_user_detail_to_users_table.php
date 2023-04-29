c7ed241f36bb7fa9d8918c39523ed33b3c55e77d
Meilina Anggraeni Putri <meilinaanggraeniputri@gmail.com>
2023-04-29 17:51:08 +0700

    user detail

---
+++ modules/UserManagement/database/migrations/2023_04_29_103853_add_user_detail_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('jabatan');
+            $table->string('area');
+            $table->string('phone', 15);
+            $table->longText('alamat');
+            $table->string('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+        });
+    }
+};
