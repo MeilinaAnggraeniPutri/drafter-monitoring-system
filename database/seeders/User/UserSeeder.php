ae490a24ab54a644b5e9f385926d99b6013a7dc5
Meilina Anggraeni Putri <meilinaanggraeniputri@gmail.com>
2023-05-03 08:23:34 +0700

    Editing userseeder

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -22,15 +22,62 @@ class UserSeeder extends Seeder
         ]);
 
         User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@gmail.com',
-            'nik' => '20951452',
+            'name' => 'Other User',
+            'email' => 'other.user@gmail.com',
+            'nik' => '2305022',
         ]);
 
         User::factory()->create([
-            'name' => 'Other User',
-            'email' => 'other.user@gmail.com',
-            'nik' => '20951453',
+            'name' => 'Denni Firmansyah',
+            'nik' => '2105092',
         ]);
+
+        User::factory()->create([
+            'name' => 'Rheza Firmansyah',
+            'nik' => '2125526',
+        ]);
+
+        User::factory()->create([
+            'name' => 'M Faqih Lukman Aji',
+            'nik' => '2095145',
+        ]);
+
+        User::factory()->create([
+            'name' => 'M Yunianto',
+            'nik' => '2084947',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Viki Kurniawan',
+            'nik' => '2095143',
+        ]);
+
+        User::factory()->create([
+            'name' => 'A Tiar Maulana',
+            'nik' => '2145980',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Digyo Niti Santoso',
+            'nik' => '2145998',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Arik Setyo Wibowo',
+            'nik' => '2095142',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Bima Arganata',
+            'nik' => '2156264',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Oki Ardianda',
+            'nik' => '2166569',
+        ]);
+
     }
+
+
 }
